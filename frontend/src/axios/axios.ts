dcb9088401b748bbe365e4398879ce344a56f938
ema <efilipovska17@gmail.com>
2026-06-19 02:31:37 +0200

    kubernetes axios test

--- frontend/src/axios/axios.ts
+++ frontend/src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+// baseURL: 'http://localhost:8080/api',
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json'
     }
